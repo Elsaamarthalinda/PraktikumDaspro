f7bc38328437aac6b7d348e006bba5e2817854fb
Elsaamarthalinda <elsa29389@gmail.com>
2025-10-05 11:52:59 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS7.java
+++ jobsheet5/ifCetakKRS7.java
@@ -7,13 +7,16 @@ public class ifCetakKRS7 {
         Scanner sc = new Scanner (System.in);
 
         boolean uktLunas;
+        
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         uktLunas = sc.nextBoolean();
 
         if (uktLunas) {
-        System.out.println("Pembayaran UKT terverivikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+            System.out.println("Pembayaran UKT terverivikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+        }
     }
-}
 }
