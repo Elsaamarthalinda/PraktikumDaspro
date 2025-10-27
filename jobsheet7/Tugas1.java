5ead00fb11db98aa04e234275f269c6cd349cab9
Elsaamarthalinda <elsa29389@gmail.com>
2025-10-27 10:30:52 +0700

    Percobaan 1

---
+++ jobsheet7/Tugas1.java
@@ -0,0 +1,30 @@
+package jobsheet7;
+import java.util.Scanner;
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int totalTiket;
+        double tiket = 50000, diskon;
+
+        do {
+            System.out.print("Masukkan jumlah tiket (ketik 'batal' silahkan masukkan lagi): ");
+            total
+
+            if (jumlahTiket > 10) {
+                diskon = 0.15;
+            } else if (jumlahTiket > 4) {
+                diskon = 0.10;
+            }
+                break;
+            
+            double totalTiket = tiket * diskon;
+            double totalHarga = tiket - jumlahTiket;
+    
+            System.out.println("Total yang harus dibayar: Rp" + totalHarga);
+            sc.nextLine();
+        } while (true);
+
+        System.out.println("Semua pesanan dibatalkan");
+    }
+}
