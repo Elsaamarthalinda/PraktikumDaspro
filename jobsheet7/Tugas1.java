648816126eb5c63ac5dc7ead4faddf79e7cfe862
Elsaamarthalinda <elsa29389@gmail.com>
2025-10-27 18:21:09 +0700

    Tugas 1

--- jobsheet7/Tugas1.java
+++ jobsheet7/Tugas1.java
@@ -3,28 +3,39 @@ import java.util.Scanner;
 public class Tugas1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        int tiket = 50000, totalTiket = 0, jmlTiket;
+        double totalPenjualan = 0, diskon, total;
 
-        int totalTiket;
-        double tiket = 50000, diskon;
+        System.out.println("Harga per tiket: Rp 50.000");
 
-        do {
-            System.out.print("Masukkan jumlah tiket (ketik 'batal' silahkan masukkan lagi): ");
-            total;
+        while (true) {
+            System.out.print("Masukkan jumlah tiket yang dibeli (ketik 0 untuk selesai): ");
+            jmlTiket = sc.nextInt();
 
-            if (jumlahTiket > 10) {
-                diskon = 0.15;
-            } else if (jumlahTiket > 4) {
-                diskon = 0.10;
-            }
+            if (jmlTiket == 0) {
                 break;
-            
-            double totalTiket = tiket * diskon;
-            double totalHarga = tiket - jumlahTiket;
-    
-            System.out.println("Total yang harus dibayar: Rp" + totalHarga);
-            sc.nextLine();
-        } while (true);
+            }
+            if (jmlTiket < 0) {
+                System.out.println("Jumlah tiket tidak valid. Input ulang!");
+            continue;
+            }
 
-        System.out.println("Semua pesanan dibatalkan");
+            diskon = 0;
+            if (jmlTiket > 10)
+            diskon = 0.15;
+            else if (jmlTiket > 4)
+            diskon = 0.10;
+
+            total = jmlTiket * tiket * (1 - diskon);
+            totalPenjualan += total;
+            totalTiket += jmlTiket;
+
+            System.out.println("\n--------------------- LAPORAN PENJUALAN ---------------------");
+            System.out.println("Total tiket terjual: " + totalTiket);
+            System.out.println("Total pendapatan  : Rp " + totalPenjualan);
+
+            System.out.println("Total harga: Rp " + total);
+        }
     }
 }
