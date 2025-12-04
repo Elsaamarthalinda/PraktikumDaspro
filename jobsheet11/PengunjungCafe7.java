41979d05f725d90586b8d598f353b8c30d159ac6
Elsaamarthalinda <elsa29389@gmail.com>
2025-12-04 16:48:18 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe7.java
+++ jobsheet11/PengunjungCafe7.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class PengunjungCafe7 {
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama pengunjung:");
-        for(int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- "+namaPengunjung[i]);
+        for(String nama : namaPengunjung) {
+            System.out.println("- "+nama);
         }
     }
 
